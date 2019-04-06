f640116b3698a6803bf2d80636b457420fe4fbc3
soggytoast111 <joncurto@gmail.com>
2019-04-05 23:56:35 -0400

    V1.0

--- assets/javascript/RPS.js
+++ assets/javascript/RPS.js
@@ -17,7 +17,11 @@ var gameActive = 0
 var rpsSelect = 0
 var opponentSelect = 0
 var rpsTable = ["Rock", "Paper", "Scissors"]
-
+var rpsImage = ["assets/images/rock-t.png", "assets/images/paper-t.png", "assets/images/scissors-t.png"]
+var wins = 0
+var losses = 0
+var ties = 0
+var winLoseArray = ["It's a Tie!", "You Lose!", "You Win!"]
 //Continue Btn Event to initiate game
 $("#continue-btn").on("click", function(event) {
     event.preventDefault()
@@ -52,6 +56,7 @@ $("#continue-btn").on("click", function(event) {
     }).then(function(){
         database.ref("game-info").child(playerKey).once("value", function(snapshot) {
             if (snapshot.val().opponentKey == null) {
+                $("#modal-body").text("Waiting for Opponent...")
                 $('#player-wait-modal').modal('toggle');
                 
                 database.ref("game-info").on("child_changed", function(snapshot) {
@@ -102,7 +107,8 @@ function selection(RPS) {
     if (gameActive == 1) {
         rpsSelect = RPS
         database.ref("game-info").child(playerKey).update({
-            "selection": RPS
+            "selection": RPS,
+            "playAgain": "wait"
         })
 
 
@@ -158,9 +164,134 @@ $("#scissors-btn").on("click", function(event) {
 })
 
 function checkWinner() {
+    
+    if(rpsSelect == opponentSelect) {
+        winLose = 0
+        ties++
+    }
+
+    else if (rpsSelect - opponentSelect == -1 || rpsSelect - opponentSelect == 2 ) {
+        winLose = 1
+        losses++
+    }
+
+    else if (rpsSelect - opponentSelect == -2 || rpsSelect - opponentSelect == 1) {
+        winLose = 2
+        wins++
+    } 
     setTimeout(function(){
-        $("#yourChoice").text("You Chose:  " + rpsTable[rpsSelect])
-        $("#opponentChoice").text("Your opponent Chose:  " + rpsTable[opponentSelect])
-        $("#jumbotron").fadeIn(1000)
-        }, 1000)
+            $("#rpsHead").text("Rock, Paper, Scissors... SHOOT!")
+            $("#ties").hide()
+            $("#yourChoice").text("You Chose:  " + rpsTable[rpsSelect])
+            $("#your-img").attr("src", rpsImage[rpsSelect]).show()
+            $("#opponentChoice").text("Your Opponent Chose:  " + rpsTable[opponentSelect])
+            $("#opponent-img").attr("src", rpsImage[opponentSelect]).show()
+            $("#paButton").hide()
+            $("#resetButton").hide()
+            $("#jumbotron").fadeIn(1000)
+            }, 1000)
+
+    setTimeout(function(){  
+        $("#jumbotron").fadeOut(1000,function(){
+            $("#rpsHead").text(winLoseArray[winLose])
+            $("#yourChoice").text("Wins:  " + wins)
+            $("#your-img").hide()
+            $("#opponentChoice").text("Losses:  " + losses)
+            $("#opponent-img").hide()
+            $("#ties").text("Ties:  " + ties).show()
+            $("#hr").hide()
+            $("#paButton").show()
+            $("#resetButton").show()
+            $("#jumbotron").fadeIn(1000)
+        })
+    }, 7000)
+}
+
+$("#paButton").click(function(event){
+    event.preventDefault()
+    database.ref("game-info").child(playerKey).update({
+        "playAgain": "yes"
+    }).then(
+        database.ref("game-info").child(opponentKey).child("playAgain").once("value", function(snapshot){
+            console.log("Opponent PlayAgain:" + snapshot.val())
+            if(snapshot.val() == "yes") {
+                softreset()
+            }
+
+            else if (snapshot.val() == "wait") {
+                $('#modal-body').text("Waiting for Opponent...")
+                $('#player-wait-modal').modal('toggle');
+                
+                database.ref("game-info").child(opponentKey).on("child_changed",function(snapshot){
+                    console.log("Opponent-Play:  " + snapshot.val())
+                    if (snapshot.val() == "yes") {
+                        database.ref("game-info").child(playerKey).update({
+                            "playAgain": "wait"
+                        })
+                        database.ref().off()
+                        $('#player-wait-modal').modal('toggle');
+                        softreset()
+                    }
+
+                    else if(snapshot.val() == "no") {
+                        database.ref("game-info").child(playerKey).update({
+                            "playAgain": "wait"
+                        })
+                        $('#player-wait-modal').modal('toggle');
+                        alert("Opponent has disconnected!")
+                        reset()
+                    }
+
+                })
+            }
+        
+            else if(snapshot.val() == "no") {
+                database.ref("game-info").child(playerKey).update({
+                    "playAgain": "wait"
+                })
+                alert("Opponent has disconnected!")
+                reset()
+            }
+        
+        })
+    )
+
+})
+
+$("#resetButton").click(function(event){
+    event.preventDefault()
+    database.ref("game-info").child(playerKey).update({
+        "playAgain": "no"
+    })
+    reset()
+})
+
+function reset() {
+    playerKey = ""
+    opponentKey = ""
+    opponentName = ""
+    gameActive = 0
+    rpsSelect = 0
+    opponentSelect = 0
+
+    $("#jumbotron").toggle()
+    $("#your-img").toggle()
+    $("#opponent-img").toggle()
+    $("resetButton").toggle()
+    $("paButton").toggle()
+    $("#top-image").fadeIn(1000)
+    $("#title").fadeIn(1000)
+    $("#input").fadeIn(1000)
+    $("#continue-btn").fadeIn(1000)
+}
+
+function softreset() {
+    database.ref("game-info").child(playerKey).child("selection").remove()
+    $("#jumbotron").hide()
+    $("#title").text("Opponent: " + opponentName)
+    $("#title").fadeIn(1000)
+    $("#rock-btn").fadeIn(1000)
+    $("#paper-btn").fadeIn(1000)
+    $("#scissors-btn").fadeIn(1000)
+    gameActive = 1
 }
